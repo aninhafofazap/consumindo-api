bfb1c5fa89be70e486fa635252a4886830248711
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-19 14:52:54 -0300

    Consumindo API de cep

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-const consultaCEP = fetch(' viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe!')
+        } else
+            console.log(r)
+    })
+    .catch(erro => console.log(erro));
 
-console.log(consultaCEP)
+console.log(consultaCEP);
