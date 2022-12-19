b68a39408c4a614283663f876877a746d9c833eb
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-19 15:58:41 -0300

    Construindo funções assincronas com async, criando requisições com await, tratando erros com try e catch e implementando varias requisições com promise all

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe!')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído'));
+async function buscaEndereco(cep) {
+    try {
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida) {
+            throw Error('CEP inválido!')
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+let ceps = ['01001000', '01001001'];
+let conjutoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjutoCeps).then(respostas => console.log(respostas))
 
-console.log(consultaCEP);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .then(resposta => resposta.json())
+//     .then(r => {
+//         if (r.erro) {
+//             throw Error('Esse cep não existe!')
+//         } else
+//             console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('Processamento concluído'));
